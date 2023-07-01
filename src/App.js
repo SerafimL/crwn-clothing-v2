9a2c217cf7b0c6087024e7e744d822fb85cdcf6c
Lucas Serafim <lucasserafim.salesforce@gmail.com>
2023-06-30 21:19:58 -0300

    lesson-5 component categoryList

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
 import './categories.scss'
-import CategoryItem from './components/categoryItem/categoryItem';
+import CategoryList from './components/categoryList/categoryList';
 
 const App = () => {
-
-	const categoryList = [
-		{
-		  "id": 1,
-		  "title": "hats",
-		  "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-		},
-		{
-		  "id": 2,
-		  "title": "jackets",
-		  "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-		},
-		{
-		  "id": 3,
-		  "title": "sneakers",
-		  "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-		},
-		{
-		  "id": 4,
-		  "title": "womens",
-		  "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-		},
-		{
-		  "id": 5,
-		  "title": "mens",
-		  "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-		}
-	 ];	 
-
 	return (
-		<div className='categories-container'>
-			{categoryList.map(({id, title, imageUrl}) => (
-				<CategoryItem
-					key={id}
-					title={title}
-					imageUrl={imageUrl}
-				/>
-			))}
-		</div>
+		<CategoryList />
 	);
 };
 
