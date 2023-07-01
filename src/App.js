4f06e47e6cfcb37fc472bd0fd92d59a4871bb90c
Lucas Serafim <lucasserafim.salesforce@gmail.com>
2023-06-30 21:02:05 -0300

    lesson-4 turning cateryItem into a componet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import './categories.scss'
+import CategoryItem from './components/categoryItem/categoryItem';
 
 const App = () => {
 
 	const categoryList = [
-		{ title: 'Hats', id: 1, },
-		{ title: 'Jackets', id: 2, },
-		{ title: 'Sneakers', id: 3, },
-		{ title: 'Womens', id: 4, },
-		{ title: 'Mans', id: 5, },
-	];
+		{
+		  "id": 1,
+		  "title": "hats",
+		  "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+		},
+		{
+		  "id": 2,
+		  "title": "jackets",
+		  "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+		},
+		{
+		  "id": 3,
+		  "title": "sneakers",
+		  "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+		},
+		{
+		  "id": 4,
+		  "title": "womens",
+		  "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+		},
+		{
+		  "id": 5,
+		  "title": "mens",
+		  "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+		}
+	 ];	 
 
 	return (
 		<div className='categories-container'>
-			{categoryList.map(({id, title}) => (
-				<div className='category-container' key={id}>
-					<div className='background-image' />
-					<div className='category-body-container'>
-						<h2>{title}</h2>
-						<p>Shop Now</p>
-					</div>
-				</div>
+			{categoryList.map(({id, title, imageUrl}) => (
+				<CategoryItem
+					key={id}
+					title={title}
+					imageUrl={imageUrl}
+				/>
 			))}
 		</div>
 	);
